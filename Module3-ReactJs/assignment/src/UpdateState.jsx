3aa0e733a9a62df0dd8b6d4948dfff1a7e9311ab
Jatin-Panesiya <jatinpanesiya2003@gmail.com>
2023-09-01 20:43:59 +0530

    Module  3 done

--- Module3-ReactJs/assignment/src/UpdateState.jsx
+++ Module3-ReactJs/assignment/src/UpdateState.jsx
@@ -10,6 +10,7 @@ const UpdateState = () => {
         <button onClick={()=>setNum(num +1)}>Increase</button>
         <h1>{num}</h1>
         <button onClick={()=>setNum(num -1)}>Decrease</button>
+        <button onClick={()=>setNum(0)}>Reset</button>
     </div>
   )
 }
